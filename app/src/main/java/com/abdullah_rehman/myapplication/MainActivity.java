81ccf9051d99eadf62ccef9f03e768b65542e200
Abdullah Rehman <rehmanian@hotmail.com>
2017-11-07 16:15:32 +0500

    Initial commit

---
+++ app/src/main/java/com/abdullah_rehman/myapplication/MainActivity.java
@@ -0,0 +1,28 @@
+package com.abdullah_rehman.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tv1,tv2,tv3;
+    EditText et1,et2;
+    Button btn;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        et1 = (EditText) findViewById(R.id.edittext1);
+        et2= (EditText) findViewById(R.id.et2);
+        tv1 = (TextView) findViewById(R.id.tv1);
+        tv2 = (TextView) findViewById(R.id.tv2);
+        tv3 = (TextView) findViewById(R.id.tv3);
+        btn = (Button) findViewById(R.id.btn);
+    }
+}
